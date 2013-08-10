3083f8da9a23b7703c86b4f0cd6d051b0c663ed7
Xiao Li <swing1979@gmail.com>
2013-08-10 08:55:36 -0700

    update example

--- examples/mingle_logs_analysis.rb
+++ examples/mingle_logs_analysis.rb
@@ -1,4 +1,5 @@
 require 'evidence'
+require 'time'
 
 include Evidence
 
@@ -13,6 +14,17 @@ mingle_log_pattern = /^
   \[tenant\:(?<tenant>[^\]]*)\]\s+
   (?<message>.*)
 $/x
+mingle_bg_log_pattern = /^
+  \w{3}\s+\d+\s+\d{2}\:\d{2}\:\d{2}\s+
+  (?<host_name>[^\s]+)\s+
+  [\w-_]+\:\s+
+  INFO\s+
+  \[(?<timestamp>[^\]]+)\]\s+
+  \[[\w-_\d]+\[(?<thread_label>[^\]]+)\]\]\s+
+  \[(?<log4j_label>[^\]]+)\]\s+
+  \[tenant\:(?<tenant>[^\]]*)\]\s+
+  (?<message>.*)
+$/x
 
 pid = lambda {|log| "#{log[:host_name]}-#{log[:thread_label]}"}
 message = lambda {|log| log[:message]}
@@ -20,29 +32,35 @@ message = lambda {|log| log[:message]}
 def parse_timestamp
   lambda do |block|
     lambda do |log|
-      #puts "[DEBUG]log[:timestamp] => #{log[:timestamp].inspect}"
       block.call(log.merge(timestamp: Time.parse(log[:timestamp])))
     end
   end
 end
 
-logs = Dir['/Users/tworker/studios/saas/log/dumpling/**/mingle-cluster*'].map do |f|
-  stream(stream(File.new(f), log_parser(mingle_log_pattern)), parse_timestamp)
+logs = Dir['./../mingle-saas/log/dumpling/**/mingle-cluster*'].reject do |f|
+  File.new(f).first(100).any? do |l|
+    l =~ mingle_bg_log_pattern
+  end
+end.map do |f|
+  stream(File.new(f), log_parser(mingle_log_pattern))
 end
+puts "[DEBUG]logs.size => #{logs.size.inspect}"
 
-require 'time'
 merged = merge_streams(logs, lambda {|log1, log2| log1[:timestamp] <=> log2[:timestamp]})
 
-counter = 0
+#counter = 0
 
-# stream(merged, lambda {|block| lambda {|log| block.call(counter += 1)}}).each do |count|
+#stream(merged, lambda {|block| lambda {|log| block.call(counter += 1)}}).each do |count|
 #   puts "#{Time.now}: #{count}"
-# end
+#end
 
 actions_stream = stream(merged, rails_action_parser(pid, message))
 
 time_window = (ARGV[0] || 60).to_i
-
-stream(actions_stream, littles_law_analysis(time_window)).each do |avg_stay_in_system|
-  puts avg_stay_in_system
+File.open('datafile', 'w') do |f|
+  stream(actions_stream, littles_law_analysis(time_window)).map do |start_time, end_time, avg_stay_in_system|
+    t1 = start_time.strftime("%m-%d %H:%M")
+    t2 = end_time.strftime("%H:%M")
+    f.write("#{t1}-#{t2} #{avg_stay_in_system}\n".tap{|r| puts r})
+  end
 end
