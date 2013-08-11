abd532e88fdf129677df3b63bba03d9427d5e22c
Xiao Li <swing1979@gmail.com>
2013-08-11 13:51:08 -0700

    update example of using littles law analysis

--- examples/mingle_logs_analysis.rb
+++ examples/mingle_logs_analysis.rb
@@ -1,5 +1,6 @@
 require 'evidence'
 require 'time'
+require 'fileutils'
 
 include Evidence
 
@@ -18,19 +19,19 @@ $/x
 pid = lambda {|log| "#{log[:host_name]}-#{log[:thread_label]}"}
 message = lambda {|log| log[:message]}
 
-logs = Dir['./log/**/*'].map do |f|
-  stream(File.new(f)) | log_parser(mingle_log_pattern)
-end
+logs = Dir['./log/**/*'].map { |f| stream(File.new(f)) | log_parser(mingle_log_pattern) }
+time_window = (ARGV[0] || 60).to_i
 puts "[DEBUG]logs.size => #{logs.size.inspect}"
+puts "[DEBUG] time_window => #{time_window.inspect}"
 
 merged_logs = merge_streams(logs, lambda {|log1, log2| log1[:timestamp] <=> log2[:timestamp]})
-result = merged_logs | rails_action_parser(pid, message) | littles_law_analysis(time_window)
+result = merged_logs | rails_action_parser(pid, message) | request_timestamp_parser | slice_stream(lambda {|action| action[:request][:timestamp]}, time_window) | littles_law_analysis
 
-time_window = (ARGV[0] || 60).to_i
-File.open('datafile', 'w') do |f|
-  result.map do |start_time, end_time, avg_stay_in_system|
-    t1 = start_time.strftime("%m-%d %H:%M")
-    t2 = end_time.strftime("%H:%M")
+FileUtils.mkdir_p('out')
+File.open('out/mingle_logs_littles_law_analysis', 'w') do |f|
+  result.map do |range, avg_stay_in_system|
+    t1 = range.min.strftime("%m-%d %H:%M")
+    t2 = range.max.strftime("%H:%M")
     f.write("#{t1}-#{t2} #{avg_stay_in_system}\n".tap{|r| puts r})
   end
 end
